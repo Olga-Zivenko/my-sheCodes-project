98f9590dc3dffc22c17d0224afa657d205b0ecb7
Olga-Zivenko <108621714+Olga-Zivenko@users.noreply.github.com>
2022-07-08 19:57:04 +0200

    change html structure and css of current weather

--- src/index.js
+++ src/index.js
@@ -22,16 +22,16 @@ function formatDate() {
   let currentData = ` ${currentDay}, ${currenthour}:${currentminute}`;
   return currentData;
 }
-let DateChange = document.querySelector("div.data");
+let DateChange = document.querySelector("#data");
 DateChange.innerHTML = formatDate(nowDate);
 
 function showWeather(response) {
-  document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+    document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("div.headCity").innerHTML = response.data.name;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#description").innerHTML =response.data.weather[0].main;
-}
+  }
 function showCurrentCity(event) {
   event.preventDefault();
   let typeCity = document.querySelector("#currentcity");
@@ -40,8 +40,8 @@ function showCurrentCity(event) {
   currentCity.innerHTML = city;
   let key = "2fe0053212ae691bfbd1ef61151dca30";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
-  axios.get(url).then(showWeather);
-}
+   axios.get(url).then(showWeather);
+  }
 let newCity = document.querySelector("form");
 newCity.addEventListener("submit", showCurrentCity);
 //🙀 Bonus point weather for geolocation
